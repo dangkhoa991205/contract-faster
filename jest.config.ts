2f932928379b997f8c33241b338a74c21e84f2ac
KHOA <dangkhoamkt.asia@gmail.com>
2026-05-07 22:13:59 +0700

    feat: scaffold Next.js 14 + shadcn/ui + Jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  testEnvironment: 'node',
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+export default createJestConfig(config)
